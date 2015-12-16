8b87bb0647ce8ca13df9fde8eb684581cedebc97
Sammy Almawaldi <almawaldi@hotmail.com>
2015-12-16 14:45:49 -0500

    Fixed some bugs

--- LabGUI/Driver.java
+++ LabGUI/Driver.java
@@ -1,4 +1,7 @@
 //Ubuntu 12.04 "Precise Pangolin"
+//Windows NT 10.0.10520 "10"
 public class Driver{
-    TempConversionWindow test = new TempConversionWindows;
+    public static void main(String[]acks){
+	TempConversionWindow test = new TempConversionWindow();
+    }
 }
--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -4,15 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class TempConversionWindow extends JFrame implements ActionListener{
-	private Container pane;
-	private JLabel j;
-	private JTextField bawks;
-	public TempConversionWindow(){
-		this.setTitle("Temperature Conversion Tool");
-		this.setSize(640, 480);
-		this.setLocation(0, 0);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+    private Container pane;
+    private JLabel j;
+    private JTextField bawks;
+    public TempConversionWindow(){
+	this.setTitle("Temperature Conversion Tool");
+	this.setSize(640, 480);
+	this.setLocation(0, 0);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e){
+	System.out.println("An event occured!");
+    }
     public static double CtoF(double c){
 	return (9.0/5.0) * c + 32;
     }
